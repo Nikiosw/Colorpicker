e5239ef85b00c3bba9544254d7b1e6c4af01ed92
Nikios Wallace <nikiosmwallace@gmail.com>
2023-10-11 15:41:49 -0400

    color function completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import "./index.css";
+
+
+const Color = (props) => {
+  return <div className={props.color}></div>
+}
 // Write your Color component here
 
 const App = () => {
@@ -7,7 +13,14 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">{
+          <div id="colors-list">
+          <Color color="red"/>
+          <Color color="green"/>
+          <Color color="blue"/>
+        </div>
+
+      }</div>
     </div>
   );
 };
